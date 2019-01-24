e611a59015b14eccf5c31c9082d8262ee1408f73
Radzivon Liovachkin <radzivon@Radzivon.Liovachkin>
2019-01-24 17:30:25 +0300

    Fetch image recognition results with retries and exponential backoff

--- src/main/java/com/demo/cli/RecognizeImagesIntoDirTask.java
+++ src/main/java/com/demo/cli/RecognizeImagesIntoDirTask.java
@@ -1,6 +1,8 @@
 package com.demo.cli;
 
 import com.demo.recognition.impl.AzureImageRecognitionService;
+import com.nurkiewicz.asyncretry.AsyncRetryExecutor;
+import com.nurkiewicz.asyncretry.RetryExecutor;
 import org.asynchttpclient.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,16 +17,25 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Collection;
 import java.util.Date;
-import java.util.Optional;
+import java.util.Objects;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
-import java.util.function.Consumer;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 import static java.lang.String.format;
 
 @CommandLine.Command(description = "Recognize FILE(s) and save results in OUTPUT_DIR folder",
         name = "image-recognition", mixinStandardHelpOptions = true, version = "0.0.1")
 public class RecognizeImagesIntoDirTask implements Callable<Void> {
+
+    private ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+
+    private RetryExecutor executor = new AsyncRetryExecutor(executorService).
+            withExponentialBackoff(500, 2).    //500ms times 2 after each retry
+            withMaxDelay(10_000).                                     //10 seconds
+            withUniformJitter().                                      //add between +/- 100 ms randomly
+            withMaxRetries(20);
 
     private static final Logger logger
             = LoggerFactory.getLogger(RecognizeImagesIntoDirTask.class);
@@ -57,75 +68,91 @@ public class RecognizeImagesIntoDirTask implements Callable<Void> {
                     logger.info("Unable to create output dir '{}'", outputDir);
                 }
             }
-            imagesToRecognize.parallelStream().forEach(image ->
-                    postImage(service, image)
-                            .thenAccept(postResult ->
-                                    postResult.ifPresent(processPostedImage(image, service))));
-            return null;
-        }
 
+            CompletableFuture[] allrequests = imagesToRecognize.parallelStream()
+                    .filter(image -> image.exists() && image.isFile())
+                    .map(File::toPath)
+                    .map(image -> postImage(service, image))
+                    .filter(Objects::nonNull)
+                    .map(imageUploadResult ->
+                            imageUploadResult.thenComposeAsync(
+                                    postResult ->
+                                            tryToFetchRecognitionResult(postResult.response, postResult.image, service)))
+                    .toArray(CompletableFuture[]::new);
+
+            //wait for all fired requests
+            CompletableFuture.allOf(allrequests).join();
+
+            return null;
+        } finally {
+            executorService.shutdown();
+        }
     }
 
-    private CompletableFuture<Optional<Response>> postImage(AzureImageRecognitionService service, File image) {
+    private CompletableFuture<ImageUploadResult> postImage(AzureImageRecognitionService service, Path image) {
         try {
-            Path imagePath = image.toPath();
-            logger.info("Processing '{}' file", imagePath);
-            if (image.exists() && image.isFile()) {
-                CompletableFuture<Response> completableFuture = service.postFile(readFileContent(image));
-                completableFuture.join();
-                return completableFuture
-                        .thenApply(Optional::of);
-            } else logger.info("Skip irregular file {}", image);
-
+            logger.info("Processing '{}' file", image);
+            return service.postFile(Files.readAllBytes(image))
+                    .thenApplyAsync(response -> new ImageUploadResult(image, response));
         } catch (IOException e) {
             logger.error("Unable to read input image file: {} ", image, e);
         }
-        return CompletableFuture.completedFuture(Optional.empty());
+        return null;
     }
 
-    private Consumer<Response> processPostedImage(File image, AzureImageRecognitionService service) {
-        return response -> {
-            if (response.getStatusCode() == 202) {
-                fetchResponse(image, response, service);
-            } else {
-                processError(image, response);
-            }
-        };
+    private CompletableFuture<Void> tryToFetchRecognitionResult(Response response, Path image, AzureImageRecognitionService service) {
+
+        if (response.getStatusCode() == 202) {
+            return executor.getFutureWithRetry(ctx -> fetchImage(response, image, service));
+        } else {
+            return processError(response);
+        }
     }
 
-    private void processError(File image, Response response) {
-        logger.error("Service wasn't able to process file: {}:\n response code: {} \n body: {} \n ",
-                image.getAbsolutePath(), response.getStatusCode(), response.getResponseBody());
+    private CompletableFuture<Void> processError(Response response) {
+        logger.error("Service wasn't able to process file: {}:\n response code: {} \n body: {} \n ", response.getStatusCode(), response.getResponseBody());
+        return CompletableFuture.completedFuture(null);
     }
 
-    private byte[] readFileContent(File image) throws IOException {
-        return Files.readAllBytes(image.toPath());
+
+    private CompletableFuture<Void> fetchImage(Response response, Path image, AzureImageRecognitionService service) {
+        Path outputFile = Paths.get(outputDir.getAbsolutePath(), outputFileName(image));
+        return service.fetchResult(response.getHeader("Operation-Location"))
+                .thenAccept(fetchResponse -> saveContentToFile(fetchResponse, outputFile));
     }
 
-    private void fetchResponse(File image, Response response, AzureImageRecognitionService service) {
-        //TODO wait and retry
-        Path filePath = Paths.get(outputDir.getAbsolutePath(), outputFileName(image));
-        CompletableFuture<Response> fetchResult = service.fetchResult(response.getHeader("Operation-Location"));
-        fetchResult.thenAccept(fetchResponse -> saveContentToFile(image, fetchResponse, filePath));
-        fetchResult.join();
-    }
-
-    private void saveContentToFile(File image, Response response, Path filePath) {
+    private void saveContentToFile(Response response, Path filePath) {
         try {
-
-            Files.write(filePath, response.getResponseBody().getBytes());
-            logger.info("Image '{}' recognition result saved to '{}'", image, filePath);
+            String responseBody = response.getResponseBody();
+            if ("{\"status\":\"Running\"}".equals(responseBody)) {
+                throw new NotReadyException();
+            }
+            Files.write(filePath, responseBody.getBytes());
+            logger.info("Recognition result saved to '{}'", filePath);
         } catch (IOException e) {
             logger.error(format("Unable to save service response into file: %s ", filePath), e);
         }
     }
 
-    private String outputFileName(File image) {
-        return format("%s_%s.json", image.getName().replace('.', '_'), currentTimeStamp());
+    private String outputFileName(Path image) {
+        return format("%s_%s.json", image.getFileName().toString().replace('.', '_'), currentTimeStamp());
     }
 
     private String currentTimeStamp() {
         DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
         return df.format(new Date());
+    }
+
+    private static final class NotReadyException extends RuntimeException {
+    }
+
+    private static final class ImageUploadResult {
+        private final Path image;
+        private final Response response;
+
+        ImageUploadResult(Path image, Response response) {
+            this.image = image;
+            this.response = response;
+        }
     }
 }
